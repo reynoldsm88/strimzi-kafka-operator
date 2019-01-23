605520af2de88091d2db6b85adfdcd0091ec29e4
Jakub Stejskal <stejskinek@gmail.com>
2019-01-23 11:31:59 +0100

    NEW: add javadoc and work in comments from review

--- systemtest/src/test/java/io/strimzi/systemtest/StrimziUpgradeST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/StrimziUpgradeST.java
@@ -165,6 +165,6 @@ public class StrimziUpgradeST extends AbstractST {
     @BeforeAll
     void createClusterOperator() {
         LOGGER.info("Creating resources before the test class");
-        createNamespaces(NAMESPACE);
+        createNamespace(NAMESPACE);
     }
 }
